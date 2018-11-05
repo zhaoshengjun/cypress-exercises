0281c2493cdd9ebb393759d01aa9e6566df73bb3
zhaoshengjun <zhao.shengjun@gmail.com>
2018-11-05 14:07:58 +1000

    add

--- cypress/integration/all.spec.js
+++ cypress/integration/all.spec.js
@@ -9,3 +9,15 @@ it("starts with zero item", () => {
 	cy.visit("localhost:3000");
 	cy.get(".todo").should("have.length", 0);
 });
+
+it.only("adds two items", () => {
+	cy.visit("localhost:3000");
+	cy.get(".new-todo")
+		.type("item 1")
+		.type("{enter}");
+	cy.contains(".todo", "item 1").should("exist");
+	cy.get(".new-todo")
+		.type("item 2")
+		.type("{enter}");
+	cy.contains(".todo", "item 2").should("exist");
+});
