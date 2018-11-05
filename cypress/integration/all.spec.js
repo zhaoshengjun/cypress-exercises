fb6bd41bb0d1d1f46f71f23c0607773932a190af
zhaoshengjun <zhao.shengjun@gmail.com>
2018-11-05 14:09:54 +1000

    beforeEach

--- cypress/integration/all.spec.js
+++ cypress/integration/all.spec.js
@@ -1,23 +1,25 @@
 /// <reference types="Cypress" />
+context("Todo MVC", () => {
+	beforeEach(() => {
+		cy.visit("localhost:3000");
+	});
 
-it("loads", () => {
-	cy.visit("localhost:3000");
-	cy.contains("h1", "todos").should("exist");
-});
+	it("loads", () => {
+		cy.contains("h1", "todos").should("exist");
+	});
 
-it("starts with zero item", () => {
-	cy.visit("localhost:3000");
-	cy.get(".todo").should("have.length", 0);
-});
+	it("starts with zero item", () => {
+		cy.get(".todo").should("have.length", 0);
+	});
 
-it.only("adds two items", () => {
-	cy.visit("localhost:3000");
-	cy.get(".new-todo")
-		.type("item 1")
-		.type("{enter}");
-	cy.contains(".todo", "item 1").should("exist");
-	cy.get(".new-todo")
-		.type("item 2")
-		.type("{enter}");
-	cy.contains(".todo", "item 2").should("exist");
+	it("adds two items", () => {
+		cy.get(".new-todo")
+			.type("item 1")
+			.type("{enter}");
+		cy.contains(".todo", "item 1").should("exist");
+		cy.get(".new-todo")
+			.type("item 2")
+			.type("{enter}");
+		cy.contains(".todo", "item 2").should("exist");
+	});
 });
